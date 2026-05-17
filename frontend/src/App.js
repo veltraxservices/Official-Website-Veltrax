4d3f9cdbcc20b09e2118494cc2a98c2092aa81e5
veltraxservices <veltrax.services@gmail.com>
2026-05-17 10:58:53 +0300

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "@/App.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "./contexts/LanguageContext";
 import { AuthProvider } from "./contexts/AuthContext";
